a27b23a9cd7cb3fadb80a18d9ef359c0b69952a4
Bollerm01 <44593676+Bollerm01@users.noreply.github.com>
2023-10-22 16:58:20 -0400

    Add files via upload

---
+++ Project1BackendCalcs.py
@@ -0,0 +1,62 @@
+
+
+
+
+
+
+'''
+AEEM4063 Project 1 Backend Calculations
+
+Computes the F, mdot, fuel flow f, and efficiencies for a real or ideal cycle HBPR Turbofan
+
+Authors: Matt Boller, Pierce Elliott
+
+University of Cincinnati FS23
+
+'''
+
+
+# Begin imports
+import numpy as np
+
+# Begin computation class
+
+class TFComputation:
+
+    def __init__(self):
+        
+        return True
+        
+    def fullCycleCalc(self):
+
+
+        return [(F/mdot), TSFC, f, thermoEff, propEff, overEff]
+     
+    def fanCalc(self):
+
+        return True
+
+    def compressCalc(self):
+        
+        return True
+    
+    def combustorCalc(self):
+
+        return True
+    
+    def turbCalc(self):
+
+        return True
+    
+    def effCalc(self):
+
+        return True
+
+    
+
+
+        
+
+
+
+
