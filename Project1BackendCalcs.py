a4cd6c62ebeb0495d97a9d2e22bce023a8742539
elliotpd <elliotpd@mail.uc.edu>
2023-10-22 19:10:25 -0400

    Combustor, Turbine, Nozzles

--- Project1BackendCalcs.py
+++ Project1BackendCalcs.py
@@ -31,7 +31,7 @@ class TFComputation:
         self.BPR = 10 # bypass ratio
         self.y_c = 1.4 # Gamma cold section
         self.y_h = 1.333 # Gamma hot section (post combustion)
-        self.T0_4 = 1560 # Turbine inlet temperature, K
+        self.T_04 = 1560 # Turbine inlet temperature, K
         self.Q = 43100 # Enthalpy of formation for fuel, kJ/kg
         self.pi_f = 1.5 # Fan pressure ratio
         self.pi_c = 36 # Compressor pressure ratio
@@ -46,6 +46,10 @@ class TFComputation:
         self.p0 = 0.227e5 # Ambient pressure - ISA table, Pa
         self.T0 = 216.8 # Ambient temperature - ISA table, K
         self.S_W = 285 # Wing area, m**2
+        self.R = 287.1 # Ideal gas constant for air, J/kg*K
+
+        self.cpa = 1.005 # Specific heat cold section, kJ/kg*K
+        self.cpg = 1.148 # Specific heat hot section, kJ/kg*K
 
         # Lift and drag calculations
         L = self.W
@@ -90,14 +94,50 @@ class TFComputation:
         return True
     
     def combustorCalc(self):
+        self.P_04 = self.P_03*self.pi_b
 
         return True
     
     def turbCalc(self):
         nm = self.n_m
+        nt = self.n_inf_t
         y = self.y_h
-
+        cpa = self.cpa
+        cpg = self.cpg
+        B = self.BPR
         
+        delta_T_HPT = cpa/(nm*cpg) * (self.T_03 - self.T_02_5)
+        self.T_04_5 = self.T_04 - delta_T_HPT
+
+        delta_T_LPT = (B+1)*cpa/(nm*cpg) * (self.T_02_5 - self.T_02)
+        self.T_05 = self.T_04_5 - delta_T_LPT
+
+        self.P_04_5 = self.P_04/(self.T_04/self.T_04_5)**(y/(nt*(y-1)))
+        
+        self.P_05 = self.P_04_5/(self.T_04_5/self.T_05)**(y/(nt*(y-1)))
+
+        return True
+    
+    def nozzleCalc(self):
+        yc = self.y_c
+        yh = self.y_h  
+        nj = self.n_j 
+        pa = self.p0
+        R = self.R   
+
+        # Fan nozzle - perfectly expanded assumption
+        M19 = np.sqrt((1/(1-nj*((pa/self.P_02_5)**((yc-1)/yc) - 1))-1)*2/(yc-1))
+        T19 = self.T_02_5/(1 + (yc-1)/2 * M19**2)
+        self.C19 = M19*np.sqrt(yc*R*T19)
+
+        # Core nozzle - perfectly expanded assumption
+        M9 = np.sqrt((1/(1-nj*((pa/self.P_05)**((yh-1)/yh) - 1))-1)*2/(yh-1))
+        T9 = self.T_05/(1 + (yh-1)/2 * M9**2)
+        self.C9 = M9*np.sqrt(yh*R*T9)
+
+        return True
+    
+    def mdotCalc(self):
 
         return True
     
