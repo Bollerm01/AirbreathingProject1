b847251f3e2e59abd24d6dcdd3a5da20f7d0f6e6
elliotpd <elliotpd@mail.uc.edu>
2023-10-22 17:37:23 -0400

    Finalized init parameters

--- Project1BackendCalcs.py
+++ Project1BackendCalcs.py
@@ -34,8 +34,27 @@ class TFComputation:
         self.Q = 43100 # Enthalpy of formation for fuel, kJ/kg
         self.pi_f = 1.5 # Fan pressure ratio
         self.pi_c = 36 # Compressor pressure ratio
-        
-        
+        self.pi_b = 0.96 # Pressure loss across the combustor
+        self.n_i = 0.98 # Inlet efficiency
+        self.n_inf_f = 0.89 # Fan polytropic efficiency
+        self.n_inf_c = 0.90 # Compressor polytropic efficiency
+        self.n_b = 0.99 # Combustor isentropic efficiency
+        self.n_inf_t = 0.90 # Turbine polytropic efficiency
+        self.n_m = 0.99 # Mechanical efficiency
+        self.n_j = 0.99 # Nozzle efficiency
+        self.p0 = 0.227e5 # Ambient pressure - ISA table, Pa
+        self.S_W = 285 # Wing area, m**2
+
+        # Lift and drag calculations
+        L = self.W
+        q = self.y_c/2 * self.p0*self.M**2
+        CL = L/(q*self.S_W)
+
+        CD = 0.056*CL**2 - 0.004*CL + 0.014
+        D = CD*q*self.S_W
+
+        self.T_r = D # Thrust required
+                
         return True
         
     def fullCycleCalc(self):
@@ -44,6 +63,7 @@ class TFComputation:
         return [(F/mdot), TSFC, f, thermoEff, propEff, overEff]
      
     def fanCalc(self):
+
 
         return True
 
