54f937b8c1b4e42cd4470b4e29cb4f253f8beb44
elliotpd <elliotpd@mail.uc.edu>
2023-11-17 17:20:17 -0500

    Init updates

--- Project 2/Project2BackendCalcs.py
+++ Project 2/Project2BackendCalcs.py
@@ -13,3 +13,27 @@ University of Cincinnati FS23
 '''
 
 # Begin the code
+class TurboMachineryComputation:
+
+    def __init__(self):
+        self.BPR = 6.0 # Bypass ratio
+        self.mdot = 280.0 # Total mass flow, kg/s
+        # Sea level static conditions
+        self.pa = 1.01325 # Ambient pressure, bar
+        self.Ta = 288.15 # Ambient temperature, K
+        self.a = 340.3 # Speed of sound, m/s
+        self.rho_a = 1.2250 # Ambient density, kg/m^3
+        # Compressor parameters
+        self.cpr = 15 # Compressor pressure ratio
+        self.n_inf_c = 0.9 # Lower bound of polytropic efficiency
+        self.pi_f = 1.4 # Fan pressure ratio
+        self.n_inf_f = 0.92 # Fan polytropic efficiency
+        self.haller = 0.65 # Lower bound on de Haller criteria
+        self.TMach_r = 1.2 # Upper bound on tip Mach number
+        # Turbine parameters
+        self.T_04 = 2275 # Turbine inlet temperature
+        self.n_inf_t = 0.92 # Turbine polytropic efficiency
+        self.phi = 0.78 # Lower bound on flow coefficient
+        self.psi = 3.3 # Upper bound on loading coefficient        
+                        
+        return
