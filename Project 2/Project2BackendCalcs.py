25adfc1843162a968d1ad389280e612d32cff7bc
Bollerm01 <bollerm81@gmail.com>
2023-11-17 16:47:35 -0500

    Creates backend calcs

---
+++ Project 2/Project2BackendCalcs.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+'''
+AEEM4063 Project 2 Backend Calculations
+
+Computes compressor and turbine states, angles, and velocities for the multi-stage compressor and turbine design
+
+Authors: Matt Boller, Pierce Elliott
+
+University of Cincinnati FS23
+
+'''
+
+# Begin the code
